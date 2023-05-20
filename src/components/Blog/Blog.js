3f77fdde0d435d8c1d9879efab51f4ab74987271
phamquang2001 <quangmk8888@gmail.com>
2023-05-20 17:53:40 +0700

    finish project

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+Blog.propTypes = {
+
+};
+
+function Blog(props) {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Blog;
